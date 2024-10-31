33e5304e60c51e7b4acfa70d0df7845ac28aa49a
cadeke <cadeke@duck.com>
2024-10-31 18:38:14 +0100

    setup react site

---
+++ site/src/App.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // Types
+  interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
+
+  // States
+  const [input, setInput] = useState("");
+  const [data, setData] = useState<User>();
+  // const URL_DEV = "http://127.0.0.1:8080";
+  const URL_PRD = "http://10.8.8.1:8080";
+
+  // Functions
+  const getData = () => {
+    const url = URL_PRD;
+    setData(undefined);
+    fetch(`${url}/user?q=${input}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  };
+
+  return (
+    <>
+      <div className="App">
+        <h1>User Search</h1>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => getData()}>Search</button>
+        {data ? (
+          <div>
+            <table>
+              <thead>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Email</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{data?.id}</td>
+                  <td>{data?.name}</td>
+                  <td>{data?.email}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
+}
+
+export default App;
